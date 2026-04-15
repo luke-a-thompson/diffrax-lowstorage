069396ad02540aea6650ebca2a49e4b6cd30061e
Luke Thompson <luke.thompson@sydney.edu.au>
2026-04-15 14:56:26 +1000

    Tweak: Clean up conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,23 @@
 from diffrax import Bosh3, Heun
 
-from diffrax_lowstorage import BWRRK33, BWRRK53, CKRK54, YRK135
+from diffrax_lowstorage import BWRRK33, BWRRK53, CKRK54, EES25, EES27, YRK135
 
-SOLVERS = [
+NO_ERROR_SOLVERS = {
     ("bwrrk33", BWRRK33),
-    ("bwrrk53", BWRRK53),
     ("yrk135", YRK135),
     ("ckrk53", CKRK54),
-]
+    ("ees25", EES25),
+    ("ees27", EES27),
+}
 
-NO_ERROR_SOLVERS = {("bwrrk33", BWRRK33), ("yrk135", YRK135), ("ckrk53", CKRK54)}
-PENULTIMATE_ERROR_SOLVERS = {("bwrrk53", BWRRK53)}
+PENULTIMATE_ERROR_SOLVERS = {
+    ("bwrrk53", BWRRK53),
+}
+
+SOLVERS = NO_ERROR_SOLVERS | PENULTIMATE_ERROR_SOLVERS
 
 BENCH_SOLVERS = [
     ("bosh3", Bosh3),
     ("heun", Heun),
-] + SOLVERS
+    *SOLVERS,
+]
